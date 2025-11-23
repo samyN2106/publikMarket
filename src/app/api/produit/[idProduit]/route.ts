f36cc131a58a7e77300362c00ef815aed84a010e
Samuel Nayo <samuelnayo161@gmail.com>
2025-11-23 17:58:30 +0000

    gestion des bugs

--- src/app/api/produit/[idProduit]/route.ts
+++ src/app/api/produit/[idProduit]/route.ts
@@ -1,12 +1,13 @@
-import { getProduits } from "@/lib/getProduits";
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function GET(request: NextRequest, { params }) {
   try {
     const idProduit = await params.idProduit;
-    const AllProduits = await getProduits();
-    const produit = AllProduits.find((p) => p.id == idProduit);
+
+    const produit = await prisma.product.findUnique({
+      where: { id: parseInt(idProduit) },
+    });
     return NextResponse.json({ produit: produit }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
